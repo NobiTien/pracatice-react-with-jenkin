74882bfa1b83f9d86632d1691d5c9a679349d285
tienlemkms <tienlem@kms-technology.com>
2021-11-12 22:39:44 +0700

    update UT to true again

--- src/app/pages/HomePage/Masthead.tsx
+++ src/app/pages/HomePage/Masthead.tsx
@@ -9,7 +9,7 @@ export function Masthead() {
   return (
     <Wrapper>
       <Logos />
-      <Title>React Boilerplate meets CRA 10</Title>
+      <Title>React Boilerplate meets CRA</Title>
       <Lead>
         Now you can use the{' '}
         <A
